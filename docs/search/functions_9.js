5614ce32602b1d6ca34b4b0fc02da95443982538
sckahn <sckahn@gmail.com>
2023-02-22 19:25:52 +0900

    [Docs] Doxygen Update

--- docs/search/functions_9.js
+++ docs/search/functions_9.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['init_0',['Init',['../class_camera_controller.html#afa2993bed40ba71f06b790c8778383f3',1,'CameraController.Init()'],['../class_detect_manager.html#a4ee4db295b737880f771405de4a550e0',1,'DetectManager.Init()'],['../class_player_movement.html#a6bf3da8be178b597f94cf6f38f0d4af3',1,'PlayerMovement.Init()'],['../class_scenario_controller.html#a4ebc2cc78c72e36e280bbabe1a0a1cc0',1,'ScenarioController.Init()']]],
-  ['interactionbutton_1',['InteractionButton',['../class_ingame_canvas_controller.html#a2f761400570fadc22f77dddcf0bede64',1,'IngameCanvasController']]],
-  ['interactiondetector_2',['InteractionDetector',['../class_detect_manager.html#a2f9eef8072e8ec01613d03e3a0072a8d',1,'DetectManager.InteractionDetector()'],['../class_detect_manager.html#a922a4d5252d9638d0a2a814dd56d4512',1,'DetectManager.InteractionDetector(List&lt; GameObject &gt; changedObjects)']]],
-  ['iteratethroughmap_3',['IterateThroughMap',['../class_detect_manager.html#ad83d84a4866870f32891fdb7a1502510',1,'DetectManager']]]
+  ['interactiondetector_1',['InteractionDetector',['../class_detect_manager.html#a2f9eef8072e8ec01613d03e3a0072a8d',1,'DetectManager.InteractionDetector()'],['../class_detect_manager.html#a922a4d5252d9638d0a2a814dd56d4512',1,'DetectManager.InteractionDetector(List&lt; GameObject &gt; changedObjects)']]],
+  ['iteratethroughmap_2',['IterateThroughMap',['../class_detect_manager.html#ad83d84a4866870f32891fdb7a1502510',1,'DetectManager']]]
 ];
